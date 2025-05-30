fb08d02d3ef3a782f08daa86ccdc472af006db93
Alex Freitas ∴ <35032234+alexsf15@users.noreply.github.com>
2025-05-29 22:31:12 -0300

    Update logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,75 +1,112 @@
+//Criado por Alexssandro de Souza freitas.
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-// Estrutura para representar um país
+#define MAX_CIDADES 10
+
+// Estrutura para representar uma cidade
 typedef struct {
+    char codigo[10];
     char nome[50];
     int populacao;
-    int area;
-    int pib;
-} Pais;
-
-// Função para criar as cartas dos países
-void criarBaralho(Pais baralho[]) {
-    strcpy(baralho[0].nome, "Brasil");
-    baralho[0].populacao = 214000000;
-    baralho[0].area = 8516000;
-    baralho[0].pib = 1900000;
-
-    strcpy(baralho[1].nome, "Estados Unidos");
-    baralho[1].populacao = 331000000;
-    baralho[1].area = 9834000;
-    baralho[1].pib = 23000000;
-
-    strcpy(baralho[2].nome, "China");
-    baralho[2].populacao = 1400000000;
-    baralho[2].area = 9597000;
-    baralho[2].pib = 17700000;
-
-    strcpy(baralho[3].nome, "Rússia");
-    baralho[3].populacao = 144000000;
-    baralho[3].area = 17098000;
-    baralho[3].pib = 1775000;
-}
-
-// Função para comparar as cartas
-int compararCartas(Pais carta1, Pais carta2, int atributo) {
-    switch (atributo) {
-        case 1: // População
-            return carta1.populacao - carta2.populacao;
-        case 2: // Área
-            return carta1.area - carta2.area;
-        case 3: // PIB
-            return carta1.pib - carta2.pib;
-        default:
-            return 0;
-    }
-}
+    float area;
+    float pib;
+} Cidade;
 
 int main() {
-    Pais baralho[4];
-    criarBaralho(baralho);
+    Cidade cidades[MAX_CIDADES];
+    int totalCidades = 0;
+    int i;
 
-    // Exemplo de jogada
-    Pais cartaJogador = baralho[0];
-    Pais cartaComputador = baralho[1];
+    printf("=== Cadastro de Cidades Super Trunfo ===\n");
 
-    printf("Sua carta: %s\n", cartaJogador.nome);
-    printf("Carta do computador: %s\n", cartaComputador.nome);
+    // Cadastro das cidades
+    printf("Quantas cidades deseja cadastrar (max %d)? ", MAX_CIDADES);
+    scanf("%d", &totalCidades);
 
+    if (totalCidades < 2 || totalCidades > MAX_CIDADES) {
+        printf("Numero invalido de cidades. Deve ser entre 2 e %d.\n", MAX_CIDADES);
+        return 1;
+    }
+
+    for (i = 0; i < totalCidades; i++) {
+        printf("\nCidade %d:\n", i + 1);
+        printf("Codigo: ");
+        scanf(" %s", cidades[i].codigo);
+        printf("Nome: ");
+        scanf(" %[^\n]", cidades[i].nome);
+        printf("Populacao: ");
+        scanf("%d", &cidades[i].populacao);
+        printf("Area (km²): ");
+        scanf("%f", &cidades[i].area);
+        printf("PIB (em bilhões): ");
+        scanf("%f", &cidades[i].pib);
+    }
+
+    // Seleção de cartas
+    int c1, c2;
+    printf("\n=== Comparação ===\n");
+    printf("Escolha duas cidades para comparar (índices de 1 a %d):\n", totalCidades);
+
+    printf("Cidade 1 (número): ");
+    scanf("%d", &c1);
+    printf("Cidade 2 (número): ");
+    scanf("%d", &c2);
+
+    // Verificação de índices válidos
+    if (c1 < 1 || c1 > totalCidades || c2 < 1 || c2 > totalCidades || c1 == c2) {
+        printf("Cidades inválidas para comparação.\n");
+        return 1;
+    }
+
+    c1--; c2--; // ajustar para índices de vetor
+
+    // Escolha do atributo
     int atributo;
-    printf("Escolha o atributo (1: População, 2: Área, 3: PIB): ");
+    printf("\nEscolha o atributo para comparar:\n");
+    printf("1 - Populacao\n");
+    printf("2 - Area\n");
+    printf("3 - PIB\n");
+    printf("Opcao: ");
     scanf("%d", &atributo);
 
-    int resultado = compararCartas(cartaJogador, cartaComputador, atributo);
+    // Comparação
+    printf("\n--- Resultado ---\n");
 
-    if (resultado > 0) {
-        printf("Você venceu a rodada!\n");
-    } else if (resultado < 0) {
-        printf("O computador venceu a rodada!\n");
-    } else {
-        printf("Empate!\n");
+    switch (atributo) {
+        case 1:
+            printf("Populacao: %s (%d) vs %s (%d)\n", cidades[c1].nome, cidades[c1].populacao, cidades[c2].nome, cidades[c2].populacao);
+            if (cidades[c1].populacao > cidades[c2].populacao)
+                printf("Vencedora: %s\n", cidades[c1].nome);
+            else if (cidades[c2].populacao > cidades[c1].populacao)
+                printf("Vencedora: %s\n", cidades[c2].nome);
+            else
+                printf("Empate!\n");
+            break;
+
+        case 2:
+            printf("Area: %s (%.2f km²) vs %s (%.2f km²)\n", cidades[c1].nome, cidades[c1].area, cidades[c2].nome, cidades[c2].area);
+            if (cidades[c1].area > cidades[c2].area)
+                printf("Vencedora: %s\n", cidades[c1].nome);
+            else if (cidades[c2].area > cidades[c1].area)
+                printf("Vencedora: %s\n", cidades[c2].nome);
+            else
+                printf("Empate!\n");
+            break;
+
+        case 3:
+            printf("PIB: %s (%.2f bilhões) vs %s (%.2f bilhões)\n", cidades[c1].nome, cidades[c1].pib, cidades[c2].nome, cidades[c2].pib);
+            if (cidades[c1].pib > cidades[c2].pib)
+                printf("Vencedora: %s\n", cidades[c1].nome);
+            else if (cidades[c2].pib > cidades[c1].pib)
+                printf("Vencedora: %s\n", cidades[c2].nome);
+            else
+                printf("Empate!\n");
+            break;
+
+        default:
+            printf("Atributo inválido.\n");
     }
 
     return 0;
